6104c2bec95f6645bbeda716c51f10c83c90f438
Max Haifei Li <max.li@live.com>
2015-03-09 14:12:00 -0500

    Create simpleCalc.js
    everyone does his part.

---
+++ simpleCalc.js
@@ -0,0 +1,29 @@
+/* arithmetic calculation */
+
+function AaronAddition (p1, p2) {
+
+return "blah blah ";
+}
+
+function JamesSubtraction (p1, p2) {
+
+return "blah blah ";AaronAddition
+}
+
+
+function CodyMultiplication (p1, p2) {
+
+return "blah blah ";
+}
+
+
+function TomDivision (p1, p2) {
+
+return "blah blah ";
+}
+
+
+console.log(AaronAddition (40, 40));
+console.log(JamesSubtraction (40, 40));
+console.log(CodyMultiplication (40, 40));
+console.log(TomDivision (40, 40));
